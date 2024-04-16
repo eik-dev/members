9abfcc53d4ab87638d3ad0b800d72c9fbf292836
TheParadox20 <onyambusamson@gmail.com>
2024-04-17 02:10:54 +0300

    setup QR scanner

--- app/verify/page.jsx
+++ app/verify/page.jsx
@@ -1,4 +1,5 @@
 'use client'
+import { Scanner } from '@yudiel/react-qr-scanner';
 import { useEffect, useState, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 export default function Page(){
@@ -9,6 +10,7 @@ export default function Page(){
         member: '',
         date:''
     });
+    let [link, setLink] = useState('');
 
     useEffect(()=>{
         setData({
@@ -18,14 +20,44 @@ export default function Page(){
         })
     },[])
 
+    let verify = result=>{
+        if (result.includes('https://members-sooty.vercel.app/verify?id='))
+            window.location.href = result
+        else
+            alert('Invalid QR code')
+    }
+
     return(
         <Suspense>
-            <div className="mx-4">
-                <span className="bg-primary text-white px-4 py-2 block my-4 text-center text-xl font-semibold lg:w-1/2">Certificate Valid</span>
-                <h3>Certificate {id} <br /> issued to {data.name.toUpperCase()}</h3>
-                <div>Member Number: {data.member}</div>
-                <div>Date issued: {data.date}</div>
-            </div>
+            {
+                id == null?
+                <>
+                <h3>Scan QR Code</h3>
+                <Scanner
+                    onResult={(text, result) => verify(text)}
+                    onError={(error) => console.log(error?.message)}
+                    styles={{
+                        container: {
+                            height: '10%',
+                            width: '100%'
+                        },
+                        finderBorder: 0,
+                        video:{}
+                    }}
+                    options={{}}
+                    components={{
+                        audio: false,
+                    }}
+                />
+                </>
+                :
+                <div className="mx-4">
+                    <span className="bg-primary text-white px-4 py-2 block my-4 text-center text-xl font-semibold lg:w-1/2">Certificate Valid</span>
+                    <h3>Certificate {id} <br /> issued to {data.name.toUpperCase()}</h3>
+                    <div>Member Number: {data.member}</div>
+                    <div>Date issued: {data.date}</div>
+                </div>
+            }
         </Suspense>
     )
 }
